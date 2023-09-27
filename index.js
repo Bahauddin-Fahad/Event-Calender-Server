9b18ad48952bf4d0784e2626757f7f2003a0d272
bahauddin fahad <bahauddin.fahad819@gmail.com>
2023-09-27 16:11:26 +0600

    400 error handling added

--- index.js
+++ index.js
@@ -290,28 +290,44 @@ async function run() {
 
     // Get Payments from DB
     app.get("/payments", async (req, res) => {
-      const cursor = paymentsCollection.find();
-      const payments = await cursor.toArray();
-      res.status(200).json({
-        status: 200,
-        message: "Successfully Got the payment data",
-        data: payments,
-      });
+      try {
+        const cursor = paymentsCollection.find();
+        const payments = await cursor.toArray();
+        res.status(200).json({
+          status: 200,
+          message: "Successfully Got the payment data",
+          data: payments,
+        });
+      } catch (error) {
+        res.status(400).send({
+          status: 400,
+          message: "Data can't be inserted",
+          error: error.message,
+        });
+      }
     });
 
     // add a Payment to db
     app.post("/save-payment", async (req, res) => {
-      const details = req.body;
-      details.price = parseInt(details?.price);
-      details.language = details?.language.toUpperCase();
-      details.date = new Date(details.date);
-      const { insertedId } = await paymentsCollection.insertOne(details);
-      details._id = insertedId;
-      res.status(200).send({
-        status: 200,
-        message: "Successfully inserted the payment data",
-        data: details,
-      });
+      try {
+        const details = req.body;
+        details.price = parseInt(details?.price);
+        details.language = details?.language.toUpperCase();
+        details.date = new Date(details.date);
+        const { insertedId } = await paymentsCollection.insertOne(details);
+        details._id = insertedId;
+        res.status(200).send({
+          status: 200,
+          message: "Successfully inserted the payment data",
+          data: details,
+        });
+      } catch (error) {
+        res.status(400).send({
+          status: 400,
+          message: "Data can't be inserted",
+          error: error.message,
+        });
+      }
     });
   } finally {
   }
